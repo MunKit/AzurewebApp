1e7d2cd3ce4e76533c924bc06bc2ba12e429d8e2
MunKit <ngmk93@gmail.com>
2016-11-19 16:11:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-$myfile = fopen("text.txt", "r") or die("Unable to open file!");
-echo fgets($myfile);
-fclose($myfile);
+$filename = "text.txt";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $myfile = fopen($filename, "a") or die("Unable to open file!");
+    fwrite($myfile,"1 1 1 1 2");
+    fclose($myfile);
+}
+if (file_exists($filename))
+{
+  $readfile = fopen($filename, "r")or die("Unable to open file!");
+  echo fread($readfile,filesize("webdictionary.txt"));
+  fclose($readfile);
+}
 ?>
