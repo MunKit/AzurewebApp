ef81982c9e81a2f0ae58a59a40fe9b99e2bb2867
MunKit <ngmk93@gmail.com>
2016-11-19 16:30:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+
 $filename = "text.txt";
+/*
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $myfile = fopen($filename, "a") or die("Unable to open file!");
     fwrite($myfile,"1 1 1 1 2");
     fclose($myfile);
-}
+}*/
 if (file_exists($filename))
 {
   $readfile = fopen($filename, "r")or die("Unable to open file!");
