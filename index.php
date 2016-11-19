a69ed8effe9668a98c58cfc1cda60d426d9ae406
MunKit <ngmk93@gmail.com>
2016-11-20 00:11:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,17 +26,18 @@ th,td {
 <?php
 $filename = "text.txt";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $myfile = fopen($filename, "a") or die("Unable to open file!");
-    fwrite($myfile,"1 1 1 1 2");
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
+    fwrite($myfile,"number of avalible parking slot: ".$_POST["slider"]);
     fclose($myfile);
 }
+    /*
 if (file_exists($filename))
 {
   $readfile = fopen($filename, "r")or die("Unable to open file!");
   while(!feof($readfile)) 
     echo fgets($readfile) . "<br>";
   fclose($readfile);
-}
+}*/
 ?>
 </div>
 
