8a1befc17e71570e28bceac42df1f9fb1e5c1172
MunKit <ngmk93@gmail.com>
2016-11-19 19:23:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
   <link rel="stylesheet" href="style.css">
 </head>
     
-
+<style>
+table,th,td {
+  border : 1px solid black;
+  border-collapse: collapse;
+}
+th,td {
+  padding: 5px;
+}
+</style>
     
 <body>
 
